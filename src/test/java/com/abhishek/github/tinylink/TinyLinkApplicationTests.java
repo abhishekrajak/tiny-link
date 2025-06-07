89eefb75ef85efd4dd6a2d9bf4bbdc112521fdd0
Abhishek Rajak <abhishekrajak100@gmail.com>
2025-06-07 17:35:25 +0530

    Initial project setup with Spring Boot, Maven, Git attributes, and essential configuration files.

---
+++ src/test/java/com/abhishek/github/tinylink/TinyLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishek.github.tinylink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyLinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
