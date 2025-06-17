addb53845948a33c4a2db5dc928b31bfada4be7a
Avinash Gupta <41068526+Avikill22@users.noreply.github.com>
2025-06-17 23:33:49 +0530

    Adding Constants and Inject Service.

--- src/main/java/com/abhishek/github/tinylink/controller/TinyLinkController.java
+++ src/main/java/com/abhishek/github/tinylink/controller/TinyLinkController.java
@@ -4,25 +4,24 @@ import com.abhishek.github.tinylink.constant.ApiErrorCodes;
 import com.abhishek.github.tinylink.constant.ApiErrorMessages;
 import com.abhishek.github.tinylink.dto.ApiResponse;
 import com.abhishek.github.tinylink.dto.TinyLinkGenerateRequestDTO;
-import com.abhishek.github.tinylink.model.User;
 import com.abhishek.github.tinylink.service.TinyLinkService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.Optional;
 
-@RestController()
+import static com.abhishek.github.tinylink.constant.StringConstants.ApiPathConstant.*;
+
+
+@RestController
+@AllArgsConstructor
 public class TinyLinkController {
 
-    TinyLinkService tinyLinkService;
+    private final TinyLinkService tinyLinkService;
 
-    public TinyLinkController(TinyLinkService tinyLinkService) {
-        this.tinyLinkService = tinyLinkService;
-    }
-
-    @PostMapping(value = "/api/v1/tiny-link")
+    @PostMapping(value = SLASH+API+SLASH+VERSION+TINY_LINK)
     public ApiResponse<?> addTinyLink(@RequestBody TinyLinkGenerateRequestDTO tinyLinkGenerateRequest) {
         boolean isSuccess = tinyLinkService.insertTinyLink(tinyLinkGenerateRequest);
         if (isSuccess) {
@@ -32,7 +31,7 @@ public class TinyLinkController {
         }
     }
 
-    @GetMapping(value = "/{tinyCode}")
+    @GetMapping(value = SLASH + PATH_VARIABLE_TINY_CODE)
     public ResponseEntity<String> getTinyLink(@PathVariable String tinyCode) {
         Optional<String> url = tinyLinkService.getRedirectionUrl(tinyCode);
 
--- src/main/java/com/abhishek/github/tinylink/service/PrefixService.java
+++ src/main/java/com/abhishek/github/tinylink/service/PrefixService.java
@@ -2,18 +2,16 @@ package com.abhishek.github.tinylink.service;
 
 import com.abhishek.github.tinylink.model.Prefix;
 import com.abhishek.github.tinylink.repository.PrefixRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class PrefixService {
 
-    PrefixRepository prefixRepository;
-
-    public PrefixService(PrefixRepository prefixRepository) {
-        this.prefixRepository = prefixRepository;
-    }
+    private final PrefixRepository prefixRepository;
 
     List<Prefix> getAllPrefixes() {
         return prefixRepository.findAll();
