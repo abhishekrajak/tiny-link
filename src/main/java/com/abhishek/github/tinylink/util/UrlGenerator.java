0216fda783195b6278f4f5b623e5da368c813b49
Avinash Gupta <41068526+Avikill22@users.noreply.github.com>
2025-06-19 21:33:11 +0530

    using SecureRandom in place of Random for short-code generation
    Constructs a secure random number generator (RNG) implementing the default random number algorithm.
    replace Math.random with SecureRandom.

--- src/main/java/com/abhishek/github/tinylink/util/UrlGenerator.java
+++ src/main/java/com/abhishek/github/tinylink/util/UrlGenerator.java
@@ -1,5 +1,7 @@
 package com.abhishek.github.tinylink.util;
 
+import java.security.SecureRandom;
+
 import static com.abhishek.github.tinylink.constant.StringConstants.NumericConstant.INT_ZERO;
 
 public final class UrlGenerator {
@@ -9,8 +11,9 @@ public final class UrlGenerator {
 
     public static String generateShortCode(int length, String allowedChars) {
         StringBuilder shortCode = new StringBuilder();
+        SecureRandom random = new SecureRandom();
         for (int i = INT_ZERO; i < length; i++) {
-            shortCode.append(allowedChars.charAt((int) (Math.random() * allowedChars.length())));
+            shortCode.append(allowedChars.charAt(random.nextInt(allowedChars.length())));
         }
         return shortCode.toString();
     }
