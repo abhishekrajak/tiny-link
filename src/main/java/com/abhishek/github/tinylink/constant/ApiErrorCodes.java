2601e44bc8c049d0f7f65df274ba2e4a051f529f
Abhishek Rajak <abhishekrajak100@gmail.com>
2025-07-10 00:49:35 +0530

    Api Error code fix

--- src/main/java/com/abhishek/github/tinylink/constant/ApiErrorCodes.java
+++ src/main/java/com/abhishek/github/tinylink/constant/ApiErrorCodes.java
@@ -3,12 +3,14 @@ package com.abhishek.github.tinylink.constant;
 public class ApiErrorCodes {
     public static String successCode = "0000";
 
-    public static String unknownErrorCode = "TL-0000";
+    public static String unknownErrorCode = "TL_0000";
 
-    public static String tinyCodeGenerationRetryFail = "TL-0001";
+    public static String tinyCodeGenerationRetryFail = "TL_0001";
 
-    public static String prefixBelongsToOtherUser = "TL-0002";
+    public static String prefixBelongsToOtherUser = "TL_0002";
 
-    public static String tinyCodeNotFound = "TL-0404";
+    public static String tinyCodeNotFound = "TL_0404";
+
+    public static String userNotFound = "TL_0003";
 
 }
