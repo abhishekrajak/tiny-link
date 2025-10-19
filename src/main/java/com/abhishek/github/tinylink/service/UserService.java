874d6cc23f1a8e9fc9bf3c7e7ffaec516281d8d0
Abhishek Rajak <abhishekrajak100@gmail.com>
2025-10-19 17:18:27 +0530

    added @Transactional annotation in the processOAuthPostLogin method

--- src/main/java/com/abhishek/github/tinylink/service/UserService.java
+++ src/main/java/com/abhishek/github/tinylink/service/UserService.java
@@ -4,6 +4,7 @@ import com.abhishek.github.tinylink.model.User;
 import com.abhishek.github.tinylink.repository.UserRepository;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
 import java.util.Collections;
@@ -18,6 +19,7 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
+    @Transactional
     public User processOAuthPostLogin(GoogleIdToken.Payload payload) {
 
         String email = payload.getEmail();
