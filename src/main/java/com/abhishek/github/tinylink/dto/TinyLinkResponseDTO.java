d92afc4612e35a4af0decfdcdf520598d736c34a
Abhishek Rajak <abhishekrajak100@gmail.com>
2026-04-07 20:02:40 +0530

    count limit on tiny link

--- src/main/java/com/abhishek/github/tinylink/dto/TinyLinkResponseDTO.java
+++ src/main/java/com/abhishek/github/tinylink/dto/TinyLinkResponseDTO.java
@@ -1,13 +1,13 @@
 package com.abhishek.github.tinylink.dto;
 
-import com.abhishek.github.tinylink.model.TinyLink;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.Instant;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @AllArgsConstructor
 @Getter
 @Setter
@@ -16,5 +16,6 @@ public class TinyLinkResponseDTO {
     private String redirectionLink;
     private boolean isCustom;
     private Instant createdAt;
+    private Long linkCountRemaining;
 
 }
