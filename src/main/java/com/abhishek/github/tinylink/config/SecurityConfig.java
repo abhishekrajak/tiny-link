1bc63725b6208308cec04da07867e0b59735da44
abhishekrajak <abhishekrajak100@gmail.com>
2026-06-09 23:31:18 +0530

    lint fixed

--- src/main/java/com/abhishek/github/tinylink/config/SecurityConfig.java
+++ src/main/java/com/abhishek/github/tinylink/config/SecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
-import org.springframework.lang.NonNullApi;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
