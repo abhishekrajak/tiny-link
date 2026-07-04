4f77e8c8a60d2f56245fe8e9448f3e4d668416fd
Abhishek Rajak <abhishekrajak100@gmail.com>
2026-07-04 20:50:27 +0530

    added smoke test for errors (#32)

--- src/test/java/com/abhishek/github/tinylink/TinyLinkApplicationTests.java
+++ src/test/java/com/abhishek/github/tinylink/TinyLinkApplicationTests.java
@@ -2,10 +2,37 @@ package com.abhishek.github.tinylink;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestComponent;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@Testcontainers
 class TinyLinkApplicationTests {
 
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine")
+            .withDatabaseName("tiny_link_test")
+            .withUsername("test_user")
+            .withPassword("test_password");
 
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.datasource.driver-class-name", () -> "org.postgresql.Driver");
+    }
+
+    @Test
+    void contextLoads() {
+        // This method doesn't need any code inside.
+        // If the Spring context fails to start, this test will fail.
+    }
 
 }
