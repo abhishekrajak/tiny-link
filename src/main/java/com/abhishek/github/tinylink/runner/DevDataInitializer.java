8bfcf3ca90d0480775d5286cabb655d4c3357080
Abhishek Rajak <abhishekrajak100@gmail.com>
2026-07-10 22:12:00 +0530

    Remove unused dependencies and cleanup `DevDataInitializer`

--- src/main/java/com/abhishek/github/tinylink/runner/DevDataInitializer.java
+++ src/main/java/com/abhishek/github/tinylink/runner/DevDataInitializer.java
@@ -1,16 +1,7 @@
 package com.abhishek.github.tinylink.runner;
 
-import com.abhishek.github.tinylink.dto.TinyLinkGenerateRequestDTO;
-import com.abhishek.github.tinylink.model.Prefix;
-import com.abhishek.github.tinylink.model.TinyLink;
-import com.abhishek.github.tinylink.model.User;
-import com.abhishek.github.tinylink.repository.PrefixRepository;
-import com.abhishek.github.tinylink.repository.TinyLinkRepository;
-import com.abhishek.github.tinylink.repository.UserRepository;
-import com.abhishek.github.tinylink.service.TinyLinkService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -18,49 +9,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class DevDataInitializer implements CommandLineRunner {
 
-    TinyLinkService tinyLinkService;
-    TinyLinkRepository tinyLinkRepository;
-
-    UserRepository userRepository;
-
-    PrefixRepository prefixRepository;
-
     @Autowired
-    DevDataInitializer(TinyLinkRepository tinyLinkRepository, UserRepository userRepository,
-                       PrefixRepository prefixRepository, TinyLinkService tinyLinkService) {
-        this.tinyLinkRepository = tinyLinkRepository;
-        this.userRepository = userRepository;
-        this.prefixRepository = prefixRepository;
-        this.tinyLinkService = tinyLinkService;
-    }
+    DevDataInitializer() {}
 
     @Override
-    public void run(String... args) throws Exception {
-//        User user = new User();
-//        user.setEmailId("abhishekrajak100@gmail.com");
-//        user.setUserType(User.UserType.BASE);
-//        user.setCreatedAt(java.time.Instant.now());
-//        userRepository.save(user);
-//
-//        TinyLink tinyLink = new TinyLink("abcde", "https://www.github.com/abhishekrajak", user, true, "CUSTOM");
-//        tinyLinkRepository.save(tinyLink);
-//
-//        Prefix prefix = new Prefix("xoxo", user, Prefix.ReservedFor.SPECIAL);
-//        Prefix prefix2 = new Prefix("abcd", user, Prefix.ReservedFor.SPECIAL);
-//        prefixRepository.save(prefix);
-//        prefixRepository.save(prefix2);
-//
-//        boolean isPrefixAlreadyExists = tinyLinkRepository.existsPrefixConflict("xoxo");
-//        boolean isPrefixAlreadyExists2 = tinyLinkRepository.existsPrefixConflict("mango");
-//
-//        System.out.println("isPrefixAlreadyExists = " + isPrefixAlreadyExists);
-//        System.out.println("isPrefixAlreadyExists = " + isPrefixAlreadyExists2);
-//
-//        TinyLinkGenerateRequestDTO tinyLinkGenerateRequestDTO = new TinyLinkGenerateRequestDTO(
-//                "abcd", "https://www.linkedin.com", user
-//        );
-//
-//        tinyLinkService.insertTinyLink(tinyLinkGenerateRequestDTO);
+    public void run(String... args) {
+        // Nothing to do for now
     }
 
 }
