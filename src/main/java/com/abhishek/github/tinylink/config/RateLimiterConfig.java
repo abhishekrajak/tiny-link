9d67ae10b2e65fe4f6c0ac87d8f0d86c18c5f1e9
Abhishek Rajak <abhishekrajak100@gmail.com>
2026-07-18 20:17:49 +0530

    port fix

--- src/main/java/com/abhishek/github/tinylink/config/RateLimiterConfig.java
+++ src/main/java/com/abhishek/github/tinylink/config/RateLimiterConfig.java
@@ -7,17 +7,24 @@ import io.lettuce.core.api.StatefulRedisConnection;
 import io.lettuce.core.codec.ByteArrayCodec;
 import io.lettuce.core.codec.RedisCodec;
 import io.lettuce.core.codec.StringCodec;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RateLimiterConfig {
 
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
     @Bean
     public RedisClient redisClient() {
         return RedisClient.create(RedisURI.builder()
-                .withHost("localhost")
-                .withPort(6379)
+                .withHost(redisHost)
+                .withPort(redisPort)
                 .build());
     }
 
