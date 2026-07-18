e46d93260b56c016fd29145c3500226323d0e151
Abhishek Rajak <abhishekrajak100@gmail.com>
2026-07-18 20:44:17 +0530

    test cases fixed

--- src/test/java/com/abhishek/github/tinylink/TinyLinkApplicationTests.java
+++ src/test/java/com/abhishek/github/tinylink/TinyLinkApplicationTests.java
@@ -1,13 +1,19 @@
 package com.abhishek.github.tinylink;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -20,12 +26,32 @@ class TinyLinkApplicationTests {
             .withUsername("test_user")
             .withPassword("test_password");
 
+    @Container
+    static GenericContainer<?> redis =
+            new GenericContainer<>(DockerImageName.parse("redis:7.2"))
+                    .withExposedPorts(6379);
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    @Test
+    void shouldStoreValue() {
+
+        redisTemplate.opsForValue().set("name", "Abhishek");
+
+        assertEquals("Abhishek",
+                redisTemplate.opsForValue().get("name"));
+    }
+
     @DynamicPropertySource
     static void configureProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.datasource.url", postgres::getJdbcUrl);
         registry.add("spring.datasource.username", postgres::getUsername);
         registry.add("spring.datasource.password", postgres::getPassword);
         registry.add("spring.datasource.driver-class-name", () -> "org.postgresql.Driver");
+
+        registry.add("spring.data.redis.host", redis::getHost);
+        registry.add("spring.data.redis.port", redis::getFirstMappedPort);
     }
 
     @Test
